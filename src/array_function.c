c6a26a1e26fac7e4b2a95b4802ee30b5d22d6416
Бачурин Виталий ИП-515 <xlii@DESKTOP-HAA6UIS.localdomain>
2025-10-23 23:56:09 +0700

    Tasks complited

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,46 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest)  {
+    for(size_t i = 0; i < size_src1; i++){
+        dest[i] = src1[i];
+    }
+    for(size_t i = 0; i < size_src2; i++){
+        dest[size_src1 + i] = src1[i];
+    }
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
+}
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    size_t i = 0, j = 0, k = 0;
+    while (i < size_src1 && j < size_src2) {
+        if (src1[i] <= src2[j]) {
+            dest[k++] = src1[i++];
+        } else {
+            dest[k++] = src2[j++];
+        }
+    }
+    
+    while (i < size_src1) {
+        dest[k++] = src1[i++];
+    }
+    
+    while (j < size_src2) {
+        dest[k++] = src2[j++];
+    }
+}
+
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    *min = src[0];
+    *max = src[0];
+    
+    // Проходим по остальным элементам
+    for (size_t i = 1; i < size; i++) {
+        if (src[i] < *min) {
+            *min = src[i];
+        }
+        if (src[i] > *max) {
+            *max = src[i];
+        }
+    }
+}
 
