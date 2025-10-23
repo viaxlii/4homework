2bf89a191411ce9cddb3a989d67b972fd6699cb9
Бачурин Виталий ИП-515 <xlii@DESKTOP-HAA6UIS.localdomain>
2025-10-24 00:11:49 +0700

    Bugs fixed

--- src/array_function.c
+++ src/array_function.c
@@ -5,7 +5,7 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
         dest[i] = src1[i];
     }
     for(size_t i = 0; i < size_src2; i++){
-        dest[size_src1 + i] = src1[i];
+        dest[size_src1 + i] = src2[i];
     }
 
 }
